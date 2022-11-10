d528b84636f62bcac3684bf35868efc897cf1a10
OrReuben <orreuven1243@gmail.com>
2022-11-10 09:51:46 +0200

    changed hazard schema

--- models/hazardModel.js
+++ models/hazardModel.js
@@ -5,30 +5,30 @@ const HazardsSchema = new Schema(
   {
     type: {
       type: String,
-      required: [true, "The text field is required"],
+      required: true,
     },
     location: {
       type: String,
-      required: [true, "The text field is required"],
+      required: true,
     },
     date: {
       type: String,
-      required: [true, "The text field is required"],
+      required: true,
     },
     body: {
       type: String,
-      required: [true, "The text field is required"],
+      required: true,
     },
     status: {
       type: String,
-      required: [true, "The text field is required"],
+      required: true,
     },
     phone: {
       type: Number,
       required: true,
     },
     img: {
-      type: String,
+      type: Array,
       default:
         "https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg",
     },
