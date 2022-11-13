d0b571b04f1203579c6b0178c81d7faabb81e515
OrReuben <orreuven1243@gmail.com>
2022-11-13 16:00:17 +0200

    added jwt

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ mongoose
 mongoose.Promise = global.Promise;
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.json());
 
 app.use("/api", HazardRoute);
